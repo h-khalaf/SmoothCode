5b17b6b722d139695edc8d6cf42f2c95da4b324a
khha20 <khalaf.hasan@outlook.com>
2021-10-14 14:06:01 +0200

    Changed footer copyright date to dynamic

--- utilities.js
+++ utilities.js
@@ -15,6 +15,7 @@ function redirectToDashboardIfLoggedIn (req, res, next) {
 function baseModel (req, res, next) {
     res.locals.isLoggedIn = req.session.isLoggedIn
     res.locals.csrfToken = req.csrfToken()
+    res.locals.currentYear = new Date().getFullYear()
     next()
 }
 
